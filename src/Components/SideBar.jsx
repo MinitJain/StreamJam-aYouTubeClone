c0ff77be21e8fb789a26006b352b773003f03b1e
Minit <minitjain.work@gmail.com>
2025-07-11 15:55:16 +0530

    Resolve Bugs: SideBar not visible

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -4,28 +4,31 @@ import { categories } from "../utils/constants";
 const selectedCategory = "New";
 
 const SideBar = () => {
-  <Stack
-    direction="row"
-    sx={{
-      overflowY: "auto",
-      height: { sx: "auto", md: "95%" },
-      flexDirection: { md: "column" },
-      color: "white",
-    }}
-  >
-    {categories.map((category) => (
-      <button
-        className="catogery-btn"
-        style={{
-          background: category.name === selectedCategory && "#FC1503",
-          color: "white",
-        }}
-      >
-        <span>{category.icon} </span>
-        <span>{category.name} </span>
-      </button>
-    ))}
-  </Stack>;
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        overflowY: "auto",
+        height: { sx: "auto", md: "95%" },
+        flexDirection: { md: "column" },
+        color: "white",
+      }}
+    >
+      {categories.map((category) => (
+        <button
+          key={category.name}
+          className="category-btn"
+          style={{
+            background: category.name === selectedCategory ? "#FC1503" : "",
+            color: "white",
+          }}
+        >
+          <span>{category.icon}</span>
+          <span>{category.name}</span>
+        </button>
+      ))}
+    </Stack>
+  );
 };
 
 export default SideBar;
--- src/Components/index.js
+++ src/Components/index.js
@@ -3,4 +3,4 @@ export { default as Feed } from "./Feed";
 export { default as ChannelDetail } from "./ChannelDetail";
 export { default as VideoDetail } from "./VideoDetail";
 export { default as SearchFeed } from "./SearchFeed";
-export { default as SideBar } from "./SearchBar";
+export { default as SideBar } from "./SideBar";
