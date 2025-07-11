8f6917272f0597fe0ed61f288358ad918a90fcdf
Minit <minitjain.work@gmail.com>
2025-07-11 15:44:25 +0530

    Add Sidebar

---
+++ src/Components/SideBar.jsx
@@ -0,0 +1,31 @@
+import { Stack } from "@mui/material";
+import { categories } from "../utils/constants";
+
+const selectedCategory = "New";
+
+const SideBar = () => {
+  <Stack
+    direction="row"
+    sx={{
+      overflowY: "auto",
+      height: { sx: "auto", md: "95%" },
+      flexDirection: { md: "column" },
+      color: "white",
+    }}
+  >
+    {categories.map((category) => (
+      <button
+        className="catogery-btn"
+        style={{
+          background: category.name === selectedCategory && "#FC1503",
+          color: "white",
+        }}
+      >
+        <span>{category.icon} </span>
+        <span>{category.name} </span>
+      </button>
+    ))}
+  </Stack>;
+};
+
+export default SideBar;
