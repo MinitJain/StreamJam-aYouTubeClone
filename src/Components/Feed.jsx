431fcf5ca415f30dfdab62b29c8497e1f015f14c
Minit <minitjain.work@gmail.com>
2025-07-11 16:35:02 +0530

    SideBar Feature: Add styles

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import SideBar from "./SideBar";
+import Videos from "./Videos";
 
 const Feed = () => {
   return (
@@ -20,6 +21,21 @@ const Feed = () => {
         >
           copyright@2025 Minit
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{
+            color: "white",
+          }}
+        >
+          New
+          <span style={{ color: "#FC31503" }}> videos</span>
+        </Typography>
+        <Videos videos={[]} />
       </Box>
     </Stack>
   );
