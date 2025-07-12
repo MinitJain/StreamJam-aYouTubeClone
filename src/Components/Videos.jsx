5b2e09e352f10a41743cf606823df040b23727cf
Minit <minitjain.work@gmail.com>
2025-07-12 17:32:30 +0530

    Add videos

--- src/Components/Videos.jsx
+++ src/Components/Videos.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { Stack, Box } from "@mui/material";
 
 const Videos = ({ videos }) => {
-  console.log(videos);
-  return <div>videos</div>;
+  return (
+    <Stack direction="" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, index) => (
+        <Box key={index}>
+          {item.id.videoID && <VideoCard video={item} />}
+          {item.id.channelID && <ChannelCard video={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Videos;
