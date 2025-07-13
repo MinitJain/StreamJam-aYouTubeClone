0761c4340e9445f0e249646e5f4edad2cff2bbec
Minit <minitjain.work@gmail.com>
2025-07-14 00:36:58 +0530

    Add Video cards functionality

--- src/Components/VideoCard.jsx
+++ src/Components/VideoCard.jsx
@@ -11,9 +11,48 @@ import {
 } from "../utils/constants";
 
 const VideoCard = ({ video }) => {
-  console.log(video.videoId);
+  console.log(video);
+  const videoId = video?.id?.videoId;
+  const ChannelId = video?.snippet?.channelId;
+  console.log(videoId);
 
-  return <div>VideoCard</div>;
+  return (
+    <Card
+      sx={{
+        width: { xs: "100%", sm: "358px" },
+        boxShadow: "none",
+        borderRadius: 0,
+      }}
+    >
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia
+          image={video?.snippet?.thumbnails?.high?.url}
+          alt={video?.snippet?.title}
+          sx={{ width: 358, height: 180 }}
+        />
+      </Link>
+      <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+            {video?.snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+        <Link to={ChannelId ? `/Channel/${ChannelId}` : demoChannelUrl}>
+          <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+            {video?.snippet?.channelTitle || demoChannelTitle}
+            <CheckCircle
+              sx={{
+                fontsize: 10,
+                color: "gray",
+                ml: "5px",
+                verticalAlign: "middle",
+              }}
+            />
+          </Typography>
+        </Link>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default VideoCard;
